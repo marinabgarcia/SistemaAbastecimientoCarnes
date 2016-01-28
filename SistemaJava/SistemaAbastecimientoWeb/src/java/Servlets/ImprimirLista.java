d9d8637f3fb390021254c1d2b56d8e73d6d5c63f
FlO <FlO@Florencia>
2016-01-28 01:37:21 -0300

    Lista de precio poner boton de imprimir

---
+++ SistemaJava/SistemaAbastecimientoWeb/src/java/Servlets/ImprimirLista.java
@@ -0,0 +1,98 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author FlO
+ */
+public class ImprimirLista extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet ImprimirLista</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet ImprimirLista at " + request.getContextPath() + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+      //  processRequest(request, response);
+        String lista="proveedor";
+        String lista2=request.getParameter("tipoPrecio");
+        if (lista.equals(lista2)){
+        long cuit= Long.parseLong(request.getParameter("proveedores"));
+       // Long.parseLong(request.getParameter("clientes"));
+        request.setAttribute("cuit", cuit);
+        request.getRequestDispatcher("ImprimirListaP.jsp").forward(request, response);
+        }else{
+        long tipoCliente= Long.parseLong(request.getParameter("clientes"));
+        request.setAttribute("tipoCliente", tipoCliente);
+        request.getRequestDispatcher("ImprimirListaC.jsp").forward(request, response);
+                    }
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
